cf11b2523ebef776b17004ed5e5b95d139fb297f
RiedleroD <github@riedler.wien>
2024-07-03 17:25:38 +0200

    disabled compact user card in mobile layout

--- src/components/user_card/user_card.js
+++ src/components/user_card/user_card.js
@@ -119,6 +119,7 @@ export default {
     },
     compactUserInfo () {
       return this.$store.getters.mergedConfig.compactUserInfo
+        && (this.$store.state.interface.layoutType !== 'mobile')
     },
     ...mapGetters(['mergedConfig'])
   },
